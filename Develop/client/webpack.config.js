d40339dbec82c3ddb355597751f6bf1fbf0f53cd
Cyrus Bigham <bighamcyrus@gmail.com>
2022-08-13 11:22:07 -0700

    installs and service worker working

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+const {GenerateSW } = require( 'workbox-webpack-plugin');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
